a18b5639ec9510cc783ed5b2775d60a0349297eb
Hack Reactor Student <student@hackreactor.com>
2017-04-06 18:59:13 -0700

    Update nqueens solver methods with partial implementations

--- src/solvers.js
+++ src/solvers.js
@@ -15,65 +15,143 @@
 
 
 
-window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-  var boardSize = n;
-  var board = new Board({'n': boardSize});
-
-// For n rows in board
-  //var rowNum = 0;
-  var pieceCount = 0;
-
-  var recurse = function(row){    
-    if (pieceCount === boardSize) {
-      //console.log(board);
-      //return board;
-      solution = board.attributes;
-    }else{
-      for (var col = 0; col < boardSize; col++) {
-        board.togglePiece(row, col);
-        if(!board.hasColConflictAt(col)){
-          pieceCount++;
-          break;
-        }else{ 
-          board.togglePiece(row, col);
-          continue;
-        }
-      }
-      row++
-      recurse(row);
-    }
+window.findNRooksSolution = function(n, possibility) {
+  var solution = false;
+  var board = new Board(possibility);
+  if(!board.hasAnyColConflicts()){
+    solution = board;
+    //console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   }
-  
-  recurse(0,);
-  //console.log(pieceCount);
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-  // for each piece placement in first row
-    // pass version of first row findRooksNSolution
-      // find other row possibilities based on that
+  var solutionCount = 0;
+  
+  var findRowPossibilities = function(){
+    var rowPossibilities = [];
+
+    // Produce all posibilities of placing of placing 1 in a row of size n
+    for(var i=0; i<n; i++){
+      var row = [];
+      for(var j=0; j<n; j++){
+        row.push(0);
+      }
+      row[i] = 1; 
+      rowPossibilities.push(row);
+    }
+
+    return rowPossibilities;
+  }
+
+
+
+  var findBoardPossibilities = function(n, rowPossibilities) {
+    var boardPossibilities = [];
+
+    function findBoardPossibility(rowsLeft, result){
+      // when you cover all the rows
+      // push to the boardPossibilities
+      if (rowsLeft === 0) {
+        boardPossibilities.push(result);
+        return;
+      }
+
+      rowPossibilities.forEach(function(row){
+        //result.push(row);
+        //concat returns the entire array
+        findBoardPossibility(rowsLeft-1, result.concat([row]))
+      })
+    }
+
+    findBoardPossibility(n, []); // give it a starting point
+
+    return boardPossibilities;
+  }
+
+  var possibilities = findBoardPossibilities(n, findRowPossibilities(n));
+
+  for(var i=0; i<possibilities.length; i++){
+    if(findNRooksSolution(n, possibilities[i]) !== false){
+      solutionCount++;
+    }
+  }
+
+
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
-// return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+
+
+
+
+// return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
+window.findNQueensSolution = function(n, possibility) {
+  var solution = false;
+  var board = new Board(possibility);
+  if(!board.hasAnyQueensConflicts()){
+    solution = board;
+    //console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  }
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+
+    var findRowPossibilities = function(){
+    var rowPossibilities = [];
+
+    // Produce all posibilities of placing of placing 1 in a row of size n
+    for(var i=0; i<n; i++){
+      var row = [];
+      for(var j=0; j<n; j++){
+        row.push(0);
+      }
+      row[i] = 1; 
+      rowPossibilities.push(row);
+    }
+
+    return rowPossibilities;
+  }
+
+
+
+  var findBoardPossibilities = function(n, rowPossibilities) {
+    var boardPossibilities = [];
+
+    function findBoardPossibility(rowsLeft, result){
+      // when you cover all the rows
+      // push to the boardPossibilities
+      if (rowsLeft === 0) {
+        boardPossibilities.push(result);
+        return;
+      }
+
+      rowPossibilities.forEach(function(row){
+        //result.push(row);
+        //concat returns the entire array
+        findBoardPossibility(rowsLeft-1, result.concat([row]))
+      })
+    }
+
+    findBoardPossibility(n, []); // give it a starting point
+
+    return boardPossibilities;
+  }
+
+  var possibilities = findBoardPossibilities(n, findRowPossibilities(n));
+
+  for(var i=0; i<possibilities.length; i++){
+    if(findNQueensSolution(n, possibilities[i]) !== false){
+      solutionCount++;
+    }
+  }
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
-  return solutionCount;
+  return solutionCount; 
 };
